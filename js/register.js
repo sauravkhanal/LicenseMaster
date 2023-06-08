306412798ec61a259e58c7cbfb45d3719c2fea32
sauravkhanal <hi.sauravkhanal@gmail.com>
2023-06-09 00:01:02 +0545

    fix: check if input field are filled before register email format is verified before submit password must be greater than 6 char email and password cannot be empty name optional till now

--- js/register.js
+++ js/register.js
@@ -1,7 +1,7 @@
 
     // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-    import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
+    
     import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js"
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,8 +21,7 @@
 
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-    const auth = getAuth();
+    const auth = getAuth(app);
     // console.log(app);
     // console.log(analytics);
     // console.log(auth)
@@ -50,34 +49,57 @@
     //         const errorMessage = error.message;
     //         alert(`Oops! Error occured\n ${errorCode} : ${errorMessage}`)
     //     });
-
-
+    
+    
     // });
     //create user with email and password
+    function validateEmail(email) {
+      // Regular expression for email validation
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+      // Check if the email matches the regex pattern
+      if (!emailRegex.test(email)) {
+        return 'Invalid email address.';
+      }
+    
+      return null; // No error
+    }
 
-    document.getElementById("register").addEventListener("click",function(e)  {
-        // alert(' btn clicked ');const auth = getAuth();
-        e.preventDefault()
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        
-        
-        createUserWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            alert('Account has been created successfully');
-            window.open('login.html',"_self");
+document.getElementById("register").addEventListener("click",function(e)  {
+    // alert(' btn clicked ');const auth = getAuth();
+    e.preventDefault()
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-            // Signed in 
-            const user = userCredential.user;
-            console.log(user);
-            // ...
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(`Oops error occured.\n ${errorCode} ${errorMessage}`)
-            // ..
-          });
+  if (email ==''){
+    alert('Email cannot be empty')
+  }
+  else if (validateEmail(email))  {
+    alert('invalid email')
+  }
+  else if(password == '') {
+    alert('password cannot be empty')
+  }
+  else if(password.length < 6)  {
+    alert('password must be greater than 6 characters')
+  }
+  else {
+    alert('inside')
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert('Account has been created successfully');
+      window.open('login.html',"_self");
 
-
+      // Signed in 
+      // const user = userCredential.user;
+      // console.log(user);
+      // ...
     })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(`Oops error occured.\n ${errorCode} ${errorMessage}`)
+      // ..
+    });        
+  }
+})
