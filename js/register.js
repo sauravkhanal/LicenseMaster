b8b3fe044d4498b9c632cd586411da8b19352278
saurav khanal <hi.sauravkhanal@gmail.com>
2023-06-12 23:27:21 +0545

    feat: Add popup for all error and success message check email against regular expression password length must be greater than 6 name not used

--- js/register.js
+++ js/register.js
@@ -1,105 +1,80 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-    
-    import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js"
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js"
 
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-        apiKey: "AIzaSyB5WHScYZt7yHCv3LDk8TuI9BkQgDvO0iM",
-        authDomain: "nepalengineeringmcq.firebaseapp.com",
-        databaseURL: "https://nepalengineeringmcq-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "nepalengineeringmcq",
-        storageBucket: "nepalengineeringmcq.appspot.com",
-        messagingSenderId: "1017211904451",
-        appId: "1:1017211904451:web:2e27dcc708ed7acd745d76",
-        measurementId: "G-55KZ0NVFHE"
-    };
+const firebaseConfig = {
+    apiKey: "AIzaSyB5WHScYZt7yHCv3LDk8TuI9BkQgDvO0iM",
+    authDomain: "nepalengineeringmcq.firebaseapp.com",
+    databaseURL: "https://nepalengineeringmcq-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "nepalengineeringmcq",
+    storageBucket: "nepalengineeringmcq.appspot.com",
+    messagingSenderId: "1017211904451",
+    appId: "1:1017211904451:web:2e27dcc708ed7acd745d76",
+    measurementId: "G-55KZ0NVFHE"
+};
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    // console.log(app);
-    // console.log(analytics);
-    // console.log(auth)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 
-    // create new user
+function validateEmail(email) {
+  // Regular expression for email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // document.getElementById("register").addEventListener("click",function()  {
-
-    //     alert("clicked")
-    //     const email = document.getElementById("email").value;
-    //     const password = document.getElementById("password").value;
-
-    //     createUserWithEmailAndPassword(auth, email, password)
-    //     .then((userCredential) => {
-    //     // Signed in 
-    //     alert("Account creation Successful")
-    //     window.open("loggedin.html","_self")
-    //     const user = userCredential.user;
-    //     console.log(user);
-    //     // ...
-    //     })
-    //     .catch((error) => {
-    //         const errorCode = error.code;
-    //         const errorMessage = error.message;
-    //         alert(`Oops! Error occured\n ${errorCode} : ${errorMessage}`)
-    //     });
-    
-    
-    // });
-    //create user with email and password
-    function validateEmail(email) {
-      // Regular expression for email validation
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
-      // Check if the email matches the regex pattern
-      if (!emailRegex.test(email)) {
-        return 'Invalid email address.';
-      }
-    
-      return null; // No error
-    }
-
-document.getElementById("register").addEventListener("click",function(e)  {
-    // alert(' btn clicked ');const auth = getAuth();
-    e.preventDefault()
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-  if (email ==''){
-    alert('Email cannot be empty')
+  // Check if the email matches the regex pattern
+  if (!emailRegex.test(email)) {
+    return false;
   }
-  else if (validateEmail(email))  {
-    alert('invalid email')
+
+  return null; // No error
+}
+
+const popup = document.getElementById("popup")
+
+function showPopup(text)  {
+  $("#popup-text").html(text);
+  popup.showModal()
+  // $("#popup").showModal(); 
+  // doesn't work with jquery
+
+}
+
+$("#popup-exit-btn").click( ()=>  {
+  popup.close()
+  // $("#popup").hide();
+})
+
+$("#register").click(function(e)  {
+
+  e.preventDefault()
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email == "")  {
+    showPopup("Please Enter your Email")
   }
-  else if(password == '') {
-    alert('password cannot be empty')
+  else if (!validateEmail(email) == false) {
+    showPopup("Invalid Email format !!")
   }
   else if(password.length < 6)  {
-    alert('password must be greater than 6 characters')
+    showPopup('Password must be greater than 6 characters')
   }
   else {
-    alert('inside')
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      alert('Account has been created successfully');
+    .then(() => {
+      showPopup('Account has been created successfully');
       window.open('login.html',"_self");
-
-      // Signed in 
-      // const user = userCredential.user;
-      // console.log(user);
-      // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      alert(`Oops error occured.\n ${errorCode} ${errorMessage}`)
-      // ..
+      
+      if (errorCode == "auth/email-already-in-use")
+        showPopup ("Account already exists !! <br> Please recover your password if you've forgot.");
+      else
+        showPopup(`Oops error occured.<br> ${errorCode} ${errorMessage}`)
     });        
   }
 })
+
